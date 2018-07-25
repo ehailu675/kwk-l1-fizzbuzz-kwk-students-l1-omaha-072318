47c76d752c34762023cf6153b4f0dac3a2af78ec
ehailu675 <elisabethahailu@gmail.com>
2018-07-25 16:43:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ if number % 3 == 0 and number % 5 != 0
   "Fizz"
 elsif number % 5 == 0 and number % 3 != 0
   "Buzz"
-else  (number % 3 == 0) && (number % 5 == 0)
+else  number % 3 == 0 && number % 5 == 0
   "FizzBuzz"
 end
 end 
