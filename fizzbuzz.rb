f21e1cc02a2a6bb7dbd7c53ce90abec8f4fb378c
ehailu675 <elisabethahailu@gmail.com>
2018-07-25 16:42:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def  fizzbuzz(number)
-if number % 3 == 0
+if number % 3 == 0 and number % 5 != 0
   "Fizz"
-elsif number % 5 == 0
+elsif number % 5 == 0 and number % 3 != 0
   "Buzz"
 else  (number % 3 == 0) && (number % 5 == 0)
   "FizzBuzz"
